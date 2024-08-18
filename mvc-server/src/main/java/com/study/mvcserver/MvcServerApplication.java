17ea263c812b8a1eec3ee1be6308f2ba62d47993
이재민 <rkwoals524@hanmail.net>
2024-08-18 23:55:59 +0900

    create mvc api server

--- mvc-server/src/main/java/com/study/mvcserver/MvcServerApplication.java
+++ mvc-server/src/main/java/com/study/mvcserver/MvcServerApplication.java
@@ -1,8 +1,13 @@
 package com.study.mvcserver;
 
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class MvcServerApplication {
 
@@ -10,4 +15,10 @@ public class MvcServerApplication {
         SpringApplication.run(MvcServerApplication.class, args);
     }
 
+    @GetMapping("/posts/{id}")
+    public Map<String, String> gertPosts(
+        @PathVariable("id") Long id
+    ) {
+        return Map.of("id", id.toString(), "content", "Post content is %d".formatted(id));
+    }
 }
