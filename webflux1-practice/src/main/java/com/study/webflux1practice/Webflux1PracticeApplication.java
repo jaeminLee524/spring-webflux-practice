6c0240b986450820c16d8d3991fc3938dc3fc9f5
이재민 <rkwoals524@hanmail.net>
2024-08-18 15:26:00 +0900

    Initial commit

---
+++ webflux1-practice/src/main/java/com/study/webflux1practice/Webflux1PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.study.webflux1practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux1PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webflux1PracticeApplication.class, args);
+    }
+
+}
---
+++ webflux1-practice/src/test/java/com/study/webflux1practice/Webflux1PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.webflux1practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webflux1PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
