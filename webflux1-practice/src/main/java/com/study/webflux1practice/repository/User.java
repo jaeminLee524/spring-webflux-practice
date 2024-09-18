00adec10047b3e2215d95159897145de97d592a8
이재민 <rkwoals524@hanmail.net>
2024-09-18 23:22:43 +0900

    connection r2dbc

--- webflux1-practice/src/main/java/com/study/webflux1practice/repository/User.java
+++ webflux1-practice/src/main/java/com/study/webflux1practice/repository/User.java
@@ -6,18 +6,26 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
 @Builder
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table("users")
 public class User {
 
+    @Id
     private Long id;
     private String name;
     private String email;
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     public void assignId(long id) {
